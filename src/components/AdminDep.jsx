e435cabb507ccf6ec93d55059ba661a472d4efd7
Phil <phil.github@gmail.com>
2024-07-12 15:12:10 +0300

    add department section component

---
+++ src/components/AdminDep.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AdminDep() {
+  return (
+    <>
+      <div className="navbar">
+        <AdminNav />
+      </div>
+      <AdminSideNav />
+    </>
+  );
+}
+
+export default AdminDep;
