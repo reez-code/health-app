d173175b8cdce5fb2aa41df95db8e92dd66b3b23
Phil <phil.github@gmail.com>
2024-07-12 11:52:00 +0300

    added overflow-x hidden

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavBar from "./components/NavBar";
+
 import HomePage from "./components/HomePage";
 import Footer from "./components/Footer";
 import "./App.css";
-import Department from "./components/Department";
+
 function App() {
   return (
     <Router>
       <div className="d-flex">
-        <div className="navbar">
-          <NavBar />
-        </div>
         <div className="content flex-grow-1">
           <Routes>
             <Route path="/" element={<HomePage />} />
