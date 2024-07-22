4e839a90e0c397ba650f0271993c063cab469348
Phil <phil.github@gmail.com>
2024-07-22 21:49:51 +0300

    added a button that will link to Doctor form

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,14 +1,27 @@
 import "../App.css";
+import "../css/button.css";
+import Button from "react-bootstrap/Button";
 import AdminNav from "./AdminNav";
 import AdminSideNav from "./AdminSideNav";
 import Doctor from "./Doctor";
+import { NavLink } from "react-router-dom";
 function AdminPage() {
+  const styling = {
+    marginLeft: "80%",
+    marginTop: "5%",
+  };
+
   return (
     <>
       <div className="navbar">
         <AdminNav />
       </div>
       <AdminSideNav />
+      <NavLink to="/admin/departments">
+        <button className="hire-button" style={styling}>
+          <span>Hire Another Doctor</span>
+        </button>
+      </NavLink>
       <Doctor />
     </>
   );
